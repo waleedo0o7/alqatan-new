9bf70e6bd75b8c09bb969ebb524909787d59a6b6
waleedo0o7 <waleedhd1@gmail.com>
2024-01-11 18:57:55 +0200

    add loaders

---
+++ loaders.php
@@ -0,0 +1,41 @@
+<?php include 'layout-header.php'; ?>
+
+<?php include 'layout-intro-section.php'; ?>
+
+<div class="container my-5">
+
+    <div class="loading-container flex-column">
+
+        <br /> <br />
+
+        <svg class="sm" viewBox="25 25 50 50">
+            <circle r="20" cy="50" cx="50"></circle>
+        </svg>
+
+        <br /> <br />
+
+        <svg class="md" viewBox="25 25 50 50">
+            <circle r="20" cy="50" cx="50"></circle>
+        </svg>
+        <br /> <br />
+
+        <svg class="lg" viewBox="25 25 50 50">
+            <circle r="20" cy="50" cx="50"></circle>
+        </svg>
+
+        <br /> <br />
+
+    </div> <!-- loading-container -->
+
+
+
+
+
+
+
+
+
+ 
+</div> <!-- container -->
+
+<?php include 'layout-footer.php'; ?>
